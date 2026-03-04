32911bd7ce3c932aaf0e2012c5c8a859a947b5b0
cheroliv <cheroliv@laposte.net>
2026-03-04 17:48:04 +0100

    🔧 chore(versions): update plugin name from bakery to slider and adjust related dependencies in libs.versions.toml

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,19 @@
-pluginManagement.repositories.gradlePluginPortal()
+pluginManagement {
+    repositories.gradlePluginPortal()
+    plugins {
+        kotlin("jvm").version(extra["kotlin.version"].toString())
+        id("com.github.node-gradle.node").version(extra["node-gradle.version"].toString())
+//        id("org.gradle.toolchains.foojay-resolver-convention").version(extra["toolchains.version"].toString())
+    }
+}
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement.repositories.mavenCentral()
+
+pluginManagement.repositories{
+    mavenLocal()
+    gradlePluginPortal()
+}
+
 
 rootProject.name = "slider-gradle"
