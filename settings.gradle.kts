73efbc67fe698367e21dfc7ee8f02c9a0da81aa1
cheroliv <cheroliv@laposte.net>
2026-03-05 21:27:35 +0100

    🔧 chore(settings.gradle.kts): simplify plugin management and adjust repository configuration for cleaner structure

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,5 @@
-pluginManagement {
-    repositories.gradlePluginPortal()
-    plugins {
-        kotlin("jvm").version(extra["kotlin.version"].toString())
-        id("com.github.node-gradle.node").version(extra["node-gradle.version"].toString())
-    }
-}
-
-@Suppress("UnstableApiUsage")
-dependencyResolutionManagement.repositories.mavenCentral()
-
 pluginManagement.repositories{
     mavenLocal()
     gradlePluginPortal()
 }
-
-
 rootProject.name = "slider-gradle"
