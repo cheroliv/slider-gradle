218e7db5597dbd2ead0e03a57007c5de9d31865a
cheroliv <cheroliv@laposte.net>
2026-03-06 08:49:44 +0100

    🔧 chore(settings.gradle.kts): add Foojay toolchains resolver and adjust dependency resolution management settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,9 @@ pluginManagement.repositories{
     mavenLocal()
     gradlePluginPortal()
 }
+plugins { id("org.gradle.toolchains.foojay-resolver-convention") version ("0.8.0") }
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
+}
 rootProject.name = "slider-gradle"
