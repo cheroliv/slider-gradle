84a88ab538ea5f5559d4a331477ee565a797b870
cheroliv <cheroliv@laposte.net>
2026-03-07 04:06:41 +0100

    ✨ feat(SliderPlugin): add SliderManager with SLIDER_GROUP constant for plugin extension creation

--- slider-plugin/slider/src/main/kotlin/com/cheroliv/slider/SliderPlugin.kt
+++ slider-plugin/slider/src/main/kotlin/com/cheroliv/slider/SliderPlugin.kt
@@ -1,9 +1,19 @@
 package com.cheroliv.slider
 
+import com.cheroliv.slider.SliderManager.SLIDER_GROUP
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
+object SliderManager {
+    const val SLIDER_GROUP = "slider"
+
+}
+
 class SliderPlugin : Plugin<Project> {
     override fun apply(project: Project) {
+        val sliderExtension = project.extensions.create(
+            SLIDER_GROUP,
+            SliderExtension::class.java
+        )
     }
 }
