2dadbdf2c13716237813fba180b76560a36523de
cheroliv <cheroliv@laposte.net>
2026-03-09 23:54:27 +0100

    🔧 chore(settings.gradle.kts): remove deprecated Gradle repository URL from plugin management configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,5 @@
 pluginManagement.repositories {
     mavenLocal()
     gradlePluginPortal()
-    "https://repo.gradle.org/gradle/libs-releases/"
-        .run(::uri)
-        .run(::maven)
 }
 rootProject.name = "slider-gradle"
