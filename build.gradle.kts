b3ebafea19fa501f1246437e84a171d8b627d1dd
cheroliv <cheroliv@laposte.net>
2026-03-09 23:54:14 +0100

    🔧 fix(build.gradle.kts): remove unused repository configuration for Maven Xillio

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,5 @@
 plugins { alias(libs.plugins.slider) }
 
-repositories {
-    "https://maven.xillio.com/artifactory/libs-release/"
-        .run(::uri)
-        .run(::maven)
-}
-
 slider {
     configPath = "slides-context.yml"
         .run(::file)
