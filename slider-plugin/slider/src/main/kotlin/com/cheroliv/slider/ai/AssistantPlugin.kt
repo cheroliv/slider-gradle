38fd0744aaf7602daf6c2dc95817c312225509e2
cheroliv <cheroliv@laposte.net>
2026-03-11 03:27:55 +0100

    🔧 refactor(AssistantPlugin): streamline apply method by integrating task creation for chat models directly

--- slider-plugin/slider/src/main/kotlin/com/cheroliv/slider/ai/AssistantPlugin.kt
+++ slider-plugin/slider/src/main/kotlin/com/cheroliv/slider/ai/AssistantPlugin.kt
@@ -6,13 +6,7 @@ import org.gradle.api.Project
 
 class AssistantPlugin : Plugin<Project> {
 
-    override fun apply(project: Project) {
-        project.tasks.register("displayE3POPrompt") {
-            it.group = "slider-ai"
-            it.description = "Dislpay on console AI prompt assistant"
-            it.doFirst { AssistantManager.PromptManager.userMessageFr.let(::println) }
-        }
+    override fun apply(project: Project) =
         // Creating tasks for each model
         project.createChatTasks()
-    }
 }
