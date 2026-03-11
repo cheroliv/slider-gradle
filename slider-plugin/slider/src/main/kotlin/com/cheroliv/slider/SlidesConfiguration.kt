284572e560034d3ad213d30c9c84a418d25ff2d4
cheroliv <cheroliv@laposte.net>
2026-03-11 03:27:28 +0100

    ✨ feat(slides): add AI configuration option to SlidesConfiguration data class

--- slider-plugin/slider/src/main/kotlin/com/cheroliv/slider/SlidesConfiguration.kt
+++ slider-plugin/slider/src/main/kotlin/com/cheroliv/slider/SlidesConfiguration.kt
@@ -1,6 +1,9 @@
 package com.cheroliv.slider
 
+import com.cheroliv.slider.ai.AiConfiguration
+
 data class SlidesConfiguration(
     val srcPath: String? = null,
     val pushSlides: GitPushConfiguration? = null,
+    val ai: AiConfiguration? = null,
 )
